4d13f0f293e63490a0670e087235e221a545fa48
bishal-arodek <bishal.shaw@arodek.com>
2023-06-23 19:21:43 +0530

    will start from 47

--- src/App.js
+++ src/App.js
@@ -3,16 +3,31 @@ import {
   BrowserRouter as Router,
   Route,
   Redirect,
+  Switch,
 } from "react-router-dom/cjs/react-router-dom";
 import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 function App() {
   return (
     <Router>
-      <Route path="/" exact>
-        <Users></Users>
-      </Route>
-      <Redirect to="/" />
+      <MainNavigation />
+      <main>
+        <Switch>
+          {/* path is for the url, for which url this component will be shown and exact
+          is used to determine if exactly this match will work or not. we can also
+          use exact={true} or exact={false}. */}
+          <Route path="/" exact>
+            <Users></Users>
+          </Route>
+          <Route path="/places/new" exact>
+            <NewPlace></NewPlace>
+          </Route>
+          {/* As the code will complie from top to bottom 'Redirect' will work if no matching url is found in 'Route' */}
+          <Redirect to="/" />
+        </Switch>
+      </main>
     </Router>
   );
 }
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,17 @@
 import React from "react";
+import UsersList from "../components/UsersList";
 
 const Users = () => {
-  return <h1>Users Work</h1>;
+  const USER = [
+    {
+      id: "u1",
+      name: "Test",
+      image:
+        "https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg",
+      places: 2,
+    },
+  ];
+  return <UsersList items={USER} />;
 };
 
 export default Users;
